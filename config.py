2e7db8f3ce738c2f9bedee37d9f1a63cc847ad9d
TURX <turx2003@gmail.com>
2020-02-22 01:32:14 +0800

    fix indention of config.py

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ def init_config():
 def read_config():
     if (not os.path.exists("trash.h5")):
         init_config()
-	with open("data.json", "r") as f:
-		for line in f:
-			temp = json.loads(line)
-	return temp
+    with open("data.json", "r") as f:
+        for line in f:
+            temp = json.loads(line)
+    return temp
 
 def write_config(key, value):
     temp = read_config()
